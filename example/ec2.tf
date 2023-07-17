403d8066b6cd9aec16e7e62b127c50bbea160a57
CodingManoj <mvp2612@hotmail.com>
2023-07-16 21:31:33 -0400

    Sample

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,5 +1,5 @@
 # Creates EC2
-resource "aws_instance" "app" {
+resource "aws_instance" "app_server" {
   ami                     = "ami-0c1d144c8fdd8d690"
   instance_type           = "t3.nano"
   vpc_security_group_ids  = ["aws_security_group.allow_all.id"]
