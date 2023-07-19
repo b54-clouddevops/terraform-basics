7ab813060f17bebad81c9139e503215604b218ea
CodingManoj <mvp2612@hotmail.com>
2023-07-18 21:18:51 -0400

    Sample

--- 07-modules/ec2/data.tf
+++ 07-modules/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "DevOps-LabImage-CentOS7"
+  name_regex       = "ansible-lab-image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
   owners          = ["self"]
 }
