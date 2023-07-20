f707b05e04bda6196c9c94563f3478a9339e84f4
CodingManoj <mvp2612@hotmail.com>
2023-07-19 20:54:16 -0400

    Sample

--- 08-roboshop-setup/main.tf
+++ 08-roboshop-setup/main.tf
@@ -3,3 +3,21 @@ module "frontend" {
     COMPONENT   = "frontend"
     APP_VERSION = "0.0.2"
 }
+
+# module "mongodb" {
+#     source      = "./ec2"
+#     COMPONENT   = "mongodb"
+#     APP_VERSION = "0.0.2"
+# }
+
+# module "cart" {
+#     source      = "./ec2"
+#     COMPONENT   = "cart"
+#     APP_VERSION = "0.0.2"
+# }
+
+# module "catalogue" {
+#     source      = "./ec2"
+#     COMPONENT   = "catalogue"
+#     APP_VERSION = "0.0.2"
+# }
